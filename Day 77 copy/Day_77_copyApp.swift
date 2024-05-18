4fe2fcaf18b102463ee09dba2ef16ae45548209f
bvo24 <brianvo2020@gmail.com>
2024-05-18 19:54:17 -0400

    Initial Commit

---
+++ Day 77 copy/Day_77_copyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day_77_copyApp.swift
+//  Day 77 copy
+//
+//  Created by Brian Vo on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Day_77_copyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
